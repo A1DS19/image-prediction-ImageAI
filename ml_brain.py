c96279577d015f095af2aff6918b6fd30d0e4dce
A1DS19 <jp06171@gmail.com>
2021-09-02 06:44:35 +0100

    ml model in class

--- ml_brain.py
+++ ml_brain.py
@@ -1,17 +1,35 @@
 from imageai.Prediction import ImagePrediction
 import os
 
-execution_path = os.getcwd()
-prediction = ImagePrediction()
-prediction.setModelTypeAsSqueezeNet()  # Model
-prediction.setModelPath(
-    os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5")
-)
-prediction.loadModel()
 
-predictions, probabilities = prediction.predictImage(
-    os.path.join(execution_path, "./img/godzilla.jpg"), result_count=5
-)
+class Ml_brain:
+    def __init__(self, image_path):
+        self.image = image_path
+        self.execution_path = os.getcwd()
+        self.prediction = ImagePrediction()
+        self.prediction.setModelTypeAsSqueezeNet()  # Model
+        self.prediction.setModelPath(
+            os.path.join(
+                self.execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"
+            )
+        )
+        self.prediction.loadModel()
 
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction, " : ", eachProbability)
+    # returns 5 possibilities
+    def predict(self):
+        predictions, probabilities = self.prediction.predictImage(
+            os.path.join(self.execution_path, self.image), result_count=5
+        )
+        return predictions, probabilities
+
+    def results(self, predictions, probabilities):
+        for eachPrediction, eachProbability in zip(predictions, probabilities):
+            print(eachPrediction, " : ", eachProbability)
+
+    def run(self):
+        predictions, probabilities = self.predict()
+        self.results(predictions, probabilities)
+
+
+brain = Ml_brain("./img/giraffe.jpg")
+brain.run()
