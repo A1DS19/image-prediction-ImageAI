b0dfe29f9289982364ac8801ed97e124406d892f
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2021-09-11 01:34:00 -0600

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from PIL import Image
+from ml_brain import Ml_brain
+
+
+app = Flask(__name__)
+brain = Ml_brain()
+CORS(app)
+
+
+@app.route("/eval-img", methods=["POST"])
+@cross_origin()
+def evaluate_image():
+    img = request.files.get("file")
+    pil_img = Image.open(img)
+    pil_img.save("img.jpg")
+    result = brain.run("img.jpg")
+    return jsonify(result)
+
+
+@app.route("/test", methods=["GET"])
+@cross_origin()
+def test_app():
+    return "HELLO WORLD"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
